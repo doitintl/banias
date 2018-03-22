57e16667f32819ef2733ca886e162ef35951b40b
Aviv Laufer <aviv.laufer@gmail.com>
2018-03-22 15:05:40 +0200

    misc scripts

--- frontend/loadtest/load.sh
+++ frontend/loadtest/load.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #Vegeta from here #Vegeta from here https://github.com/tsenart/vegeta
 vegeta -cpus 4 attack -targets=targets.txt -duration=1m -rate 10000  tee /tmp/results.bin | vegeta report
