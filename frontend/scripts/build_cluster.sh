7d39b6db59ebd45d8fcf1f4554692033b12ef328
Vadim Solovey <vadim@doit-intl.com>
2018-05-13 17:00:07 +0300

    enable autoscaling for cluster

--- frontend/scripts/build_cluster.sh
+++ frontend/scripts/build_cluster.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 gcloud  container clusters create $CLUSTERNAME --cluster-version \
  "1.9.3-gke.0" --no-enable-legacy-authorization --image-type "UBUNTU" \
    --machine-type "n1-standard-2" \
    --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-   --network "default" --enable-cloud-logging --enable-cloud-monitoring --subnetwork "default"
+   --network "default" --enable-autoscaling --enable-cloud-logging --enable-cloud-monitoring --subnetwork "default"
