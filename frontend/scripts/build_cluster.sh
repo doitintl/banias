6266b32f092e18e5aca65fd7da560808492aab8f
avivl <aviv.laufer@gmail.com>
2019-01-17 11:48:05 +0200

    fix build cluster script

--- frontend/scripts/build_cluster.sh
+++ frontend/scripts/build_cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 gcloud  container clusters create $CLUSTERNAME --cluster-version \
- "1.9.7-gke.0" --no-enable-legacy-authorization --image-type "UBUNTU" \
+ "1.11.5-gke.5" --no-enable-legacy-authorization --image-type "UBUNTU" \
    --machine-type "n1-standard-4" \
    --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-   --network "default" --enable-autoscaling --enable-cloud-logging --enable-cloud-monitoring --subnetwork "default"
+   --network "default" --enable-autoscaling --enable-cloud-logging --enable-cloud-monitoring --subnetwork "default" \
+    --num-nodes "3"  --min-nodes 3 --max-nodes 10 
